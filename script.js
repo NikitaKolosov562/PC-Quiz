ddf410584f62e0de5797fbbef1e2ab498d4d157b
Nikita <nkolosov08@icloud.com>
2024-10-16 13:56:12 -0700

    PC Quiz

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 $(document).ready(function(){
 	
-		$("button").click(function(){
-  $("div").animate({left: '250px'});
-});
+		
+  $(".button1").click(function(){
+  $(".text1").toggle();
+   });
+  $(".button2").click(function(){
+  $(".text2").toggle();
+   });
+  $(".button3").click(function(){
+  $(".text3").toggle();
+   });
+  $(".button4").click(function(){
+  $(".text4").toggle();
+   });
+  $(".button5").click(function(){
+  $(".text5").toggle();
+   });
+  $(".button6").click(function(){
+  $(".text6").toggle();
+   });
+  $(".button7").click(function(){
+  $(".text7").toggle();
+   });
+  $(".button8").click(function(){
+  $(".text8").toggle();
+   });
+  $(".button9").click(function(){
+  $(".text9").toggle();
+   });
+
+
+  
+  
+  
+  
+  
+
+
+  
 });
